6abca665ab7bc4a510703cc7c4d487f80597e4b3
Satwika <155884969+satwika-55@users.noreply.github.com>
2024-09-21 09:18:22 +0530

    Update LCA.py
    added LCA code

--- LCA.py
+++ LCA.py
@@ -1,1 +1,16 @@
 
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root == p or root == q:
+            return root
+        def helper(node):
+            if not node:
+                return None
+            left = helper(node.left)
+            right = helper(node.right)
+            if node == p or node == q:
+                return node
+            if left and right:
+                return node
+            return left if left else right
+        return helper(root)
