d7a0d5b1b7389c91fdf307ebec8f45202678b43a
Satwika <155884969+satwika-55@users.noreply.github.com>
2024-09-24 15:31:28 +0530

    Update CPTemplate.py
    added template

--- CPTemplate.py
+++ CPTemplate.py
@@ -5,3 +5,45 @@
         #    #       #        #       #   #   #     #     #  #       #       #
 #       #   #         #       #       #  # #  #     #     #    #    #         #  
  #######   #           #      #       ##    ##    ######  #      # #           #  
+
+
+import os
+import sys
+import time
+import random
+import re
+import math
+import threading
+import bisect
+from sortedcontainers import SortedList
+
+# Collections and Data Structures
+from collections import Counter, defaultdict, deque
+from itertools import accumulate, combinations, permutations
+from heapq import heapify, heappop, heappush, nlargest, nsmallest
+from copy import deepcopy
+
+# Functional Programming
+from functools import cmp_to_key, lru_cache, reduce
+
+# Constants
+MOD1 = int(1e9 + 7)
+MOD2 = 998244353
+INF = int(1e17)
+inf = float('inf')
+
+# Alphabet and Vowel Lists
+alphabets = list("abcdefghijklmnopqrstuvwxyz")
+vowels = list("aeiou")
+
+# Input Functions
+I = lambda: input()
+II = lambda: int(input())
+MII = lambda: map(int, input().split())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+LGMII = lambda: map(lambda x: int(x) - 1, input().split())
+LGLII = lambda: list(map(lambda x: int(x) - 1, input().split()))
+
+
+
